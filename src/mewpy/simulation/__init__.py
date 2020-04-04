8c95c1b1c2d529a5889ee5c2c60f321e31bb9846
Vitor <vmsapereira@gmail.com>
2020-04-04 17:57:54 +0100

    Minor update

--- src/mewpy/simulation/__init__.py
+++ src/mewpy/simulation/__init__.py
@@ -1,46 +1,61 @@
 from enum import Enum
 
+# Model specific simulators mapping:
+# Entries take the form:  full_model_class_path -> (simulator_path, simulator_class_name)
 
-# maps a model to its simulator
-# entries take the form:  full_model_class_path -> (simulator_path, simulator_class_name)
-
-    
-map_model_simulator ={
-    'cobra.core.model.Model'        : ('mewpy.simulation.cobra',    'Simulation'),
-    'reframed.core.cbmodel.CBModel' : ('mewpy.simulation.reframed', 'Simulation'),
-    'geckopy.gecko.GeckoModel'      : ('mewpy.simulation.cobra',    'GeckoSimulation'),
-    'mewpy.model.gecko.GeckoModel'  : ('mewpy.simulation.reframed', 'GeckoSimulation')
+map_model_simulator = {
+    'geckopy.gecko.GeckoModel': ('mewpy.simulation.cobra',    'GeckoSimulation'),
+    'mewpy.model.gecko.GeckoModel': ('mewpy.simulation.reframed', 'GeckoSimulation')
 }
 
 
-def get_simulator(model, envcond = None , constraints = None , reference= None):
+def get_simulator(model, envcond=None, constraints=None, reference=None):
     """
     Returns a simulator instance for the model
     The simulator instance is model dependent.
-    
+
     This function is invoked by a EA optimization Problem and by evaluation function instances.
-    
+
     arguments:
         model : the model
         envcond (dic) : a dictionary of environmental conditions
         contrainsts (dic) : a dictionary of additional persistent constraints
     """
+    instance = None
     name = f"{model.__class__.__module__}.{model.__class__.__name__}"
-    try:
+    if name in map_model_simulator:
         module_name, class_name = map_model_simulator[name]
-    except Exception:
-        raise ValueError(f"The model [{name}] has no defined simulator.")
-    module = __import__(module_name,fromlist=[None])
-    class_ = getattr(module, class_name)
-    instance = class_(model, envcond = envcond , constraints = constraints, reference= reference)
+        module = __import__(module_name, fromlist=[None])
+        class_ = getattr(module, class_name)
+        instance = class_(model, envcond=envcond,
+                          constraints=constraints, reference=reference)
+    else:
+        try:
+            from cobra.core.model import Model
+            if isinstance(model, Model):
+                from .cobra import Simulator
+                instance = Simulator(
+                    model, envcond=envcond, constraints=constraints, reference=reference)
+        except:
+            pass
+        if not instance:
+            try:
+                from reframed.core.cbmodel import CBModel
+                if isinstance(model, CBModel):
+                    from .reframed import Simulator
+                    instance = Simulator(
+                        model, envcond=envcond, constraints=constraints, reference=reference)
+            except:
+                pass
+        if not instance:
+            raise ValueError(f"The model [{name}] has no defined simulator.")
     return instance
-
 
 
 def get_container(model):
     """
     Returns a container for a given model sharing a common interface
-    
+
     :param model 
     """
     try:
@@ -50,7 +65,7 @@ def get_container(model):
             return CBModelContainer(model)
     except:
         pass
-    
+
     try:
         from cobra.core.model import Model
         if isinstance(model, Model):
@@ -58,17 +73,17 @@ def get_container(model):
             return CobraModelContainer(model)
     except:
         pass
-    
-    raise ValueError("Unrecognized model")
+
+    raise ValueError(f"Unrecognized model class: {model.__class__.name}")
 
 
 class SimulationMethod(Enum):
-    FBA   = 'FBA'
-    pFBA  = 'pFBA'
-    MOMA  = 'MOMA'
+    FBA = 'FBA'
+    pFBA = 'pFBA'
+    MOMA = 'MOMA'
     lMOMA = 'lMOMA'
-    ROOM  = 'ROOM'
-    NONE  = 'NONE'
+    ROOM = 'ROOM'
+    NONE = 'NONE'
 
 
 class SStatus(Enum):
