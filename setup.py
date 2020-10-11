f898c6240f69709f27ed5a66b5751b3ba708a0cf
Vítor Pereira <vmsapereira@gmail.com>
2020-10-11 18:33:57 +0100

    Version update [0.0.9]

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.rst') as readme_file:
 
 setup(
     name='mewpy',
-    version='0.0.8',
+    version='0.0.9',
     package_dir={'': 'src'},
     packages=find_packages('src'),
     package_data={"": ["*.xml", "*.csv", "*.txt"], 'mewpy': files},
