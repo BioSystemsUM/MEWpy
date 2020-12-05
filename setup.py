d0f314d4066fc6232693652fcfe3a5a1a5b46f09
Vitor <vmsapereira@gmail.com>
2020-12-05 23:56:15 +0000

    Travis

--- setup.py
+++ setup.py
@@ -5,35 +5,32 @@ files = ["model/data/*"]
 with open('README.rst') as readme_file:
     readme = readme_file.read()
 
-#with open('HISTORY.rst') as history_file:
-#    history = history_file.read()
+requirements = ['optlang<1.4.6', 'python-libsbml', 'inspyred', 'reframed',
+                'cobra', 'cobamp', 'networkx','matplotlib<3.3']
+
+install_requirements = requirements + ['jmetalpy']
+setup_requirements = requirements + ['pytest-runner','cplex'] 
+test_requirements = requirements + ['pytest','cplex']
 
 setup(
     name='mewpy',
     version='0.1.2',
+    python_requires='>=3.6',
     package_dir={'': 'src'},
     packages=find_packages('src'),
     package_data={"": ["*.xml", "*.csv", "*.txt"], 'mewpy': files},
     include_package_data=True,
     zip_safe=False,
-    install_requires=[
-        'optlang<1.4.6',
-        'python-libsbml',
-        'inspyred',
-        'reframed',
-        'cobra',
-        'jmetalpy',
-        'cobamp',
-        'networkx'],
+    install_requires=install_requirements,
+    setup_requires=setup_requirements,
+    tests_require=test_requirements,
     author='BiSBII CEB University of Minho',
     author_email='vpereira@ceb.uminho.pt',
     description='mewpy - Metabolic Engineering in Python ',
     license='Apache License Version 2.0',
     keywords='strain optimization',
-    url='',
-    long_description=open('README.rst').read(),
-    classifiers=[
-        'Topic :: Utilities',
-        'Programming Language :: Python :: 3.7',
-    ],
+    url='https://github.com/BioSystemsUM/mewpy/',
+    long_description=readme,
+    test_suite='tests',
+    
 )
