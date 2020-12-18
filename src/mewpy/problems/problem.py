7e8426e231e7cec5a3dd04fde9129d4cf7675f54
Vítor Pereira <vitor.pereira@algoritmi.uminho.pt>
2020-12-18 14:16:38 +0000

    [UPDATE] minor

--- src/mewpy/problems/problem.py
+++ src/mewpy/problems/problem.py
@@ -9,6 +9,7 @@ import numpy as np
 from ..optimization.ea import Solution
 from ..simulation import get_simulator
 from ..util.constants import EAConstants, ModelConstants
+from ..util.process import Evaluable
 
 
 class Strategy(IntEnum):
