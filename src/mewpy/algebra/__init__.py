dfca3929408b08485cde47bc369ba8b31734df38
cruz-f <fernandocruz184@gmail.com>
2021-03-24 16:27:15 +0000

    [ADD] Algebra sub-package including parsing symbolic expressions, Expression and Symbolic

---
+++ src/mewpy/algebra/__init__.py
@@ -0,0 +1,5 @@
+from .expression import Expression
+from .symbolic import *
+
+# TODO: I have implemented an algebra sub-package that allows smooth modeling operations with variables
+#  and algebra expressions of mostly any kind. It is similar to sympy expression handling
---
+++ src/mewpy/algebra/expression.py
@@ -0,0 +1,186 @@
+from itertools import product
+from typing import Dict, Union, TYPE_CHECKING, Callable
+
+# TODO: this module depends on pandas dataframes. Should it be set as package requirement?
+# noinspection PyPackageRequirements
+from pandas import DataFrame
+
+from mewpy.util.miscellaneous import solution_decode
+from mewpy.algebra.parsing import tokenize
+from mewpy.algebra.symbolic import _walk, NoneAtom, Symbolic
+
+if TYPE_CHECKING:
+    from mewpy.variables import Gene, Metabolite, Reaction, Regulator
+
+
+# TODO: methods stubs and type hinting
+class Expression:
+
+    def __init__(self,
+                 symbolic: Symbolic = None,
+                 variables: Dict[str, Union['Gene', 'Metabolite', 'Reaction', 'Regulator']] = None):
+
+        """
+        Association between a symbolic mathematical expression and variables.
+
+        :param symbolic:
+        :param variables:
+
+        """
+
+        if symbolic is None:
+            symbolic = NoneAtom()
+
+        if variables is None:
+            variables = {}
+
+        self.symbolic: Symbolic = symbolic
+        self._variables: Dict[str, Union['Gene', 'Metabolite', 'Reaction', 'Regulator']] = {}
+
+        self.variables = variables
+
+    def check_association(self):
+
+        for symbol in self.symbols.values():
+
+            if symbol.name not in self.variables:
+                raise ValueError('Expression set with incorrect variables or symbolic expression')
+
+    @property
+    def variables(self):
+        return self._variables
+
+    @variables.setter
+    def variables(self, value):
+
+        self._variables = value
+
+        self.check_association()
+
+    @property
+    def symbols(self):
+        return {symbol.name: symbol for symbol in self.symbolic.atoms(symbols_only=True)}
+
+    def __repr__(self):
+
+        return repr(self.symbolic)
+
+    def __str__(self):
+        return str(self.symbolic)
+
+    def to_string(self):
+        return self.symbolic.to_string()
+
+    def to_tokenize(self):
+        return tokenize(self.to_string())
+
+    def __iter__(self):
+
+        return self.symbolic.__iter__()
+
+    def __next__(self):
+
+        return self.symbolic.__next__()
+
+    def walk(self, reverse=False):
+
+        return _walk(self.symbolic, reverse)
+
+    def __call__(self,
+                 values: Dict[str, int],
+                 coefficient: Union[float, int] = None,
+                 operators: Dict[Symbolic, Callable] = None,
+                 default: Union[float, int] = 0.0,
+                 decoder: dict = None,
+                 **kwargs):
+
+        return self.evaluate(values=values,
+                             coefficient=coefficient,
+                             operators=operators,
+                             default=default,
+                             decoder=decoder,
+                             **kwargs)
+
+    def evaluate(self,
+                 values: Dict[str, int],
+                 coefficient: Union[float, int] = None,
+                 operators: Dict[Symbolic, Callable] = None,
+                 default: Union[float, int] = 0.0,
+                 decoder: dict = None,
+                 **kwargs):
+
+        res = self.symbolic.evaluate(values=values, operators=operators, default=default, **kwargs)
+
+        res = solution_decode(res, decoder)
+
+        # if a coefficient is provided and the expression is evaluated to true, the respective coefficient will
+        # be returned
+        if coefficient is None:
+            return res
+
+        else:
+
+            if res:
+                return coefficient
+
+            return res
+
+    def __getattr__(self, item):
+
+        try:
+            return getattr(self.symbolic, item)
+
+        except AttributeError:
+
+            raise AttributeError
+
+    def truth_table(self,
+                    values: Dict[str, Union[float, int]] = None,
+                    active_states: bool = True,
+                    coefficient: Union[float, int] = None,
+                    operators: Dict[Symbolic, Callable] = None,
+                    default: Union[float, int] = 0.0,
+                    decoder: dict = None):
+
+        truth_table = []
+
+        if values:
+
+            values['result'] = self.evaluate(values=values,
+                                             coefficient=coefficient,
+                                             operators=operators,
+                                             default=default,
+                                             decoder=decoder)
+
+            truth_table.append(values)
+
+        else:
+            if active_states:
+                values = {key: variable.coefficient.active_coefficient
+                          for key, variable in self.variables.items()}
+
+                values['result'] = self.evaluate(values=values,
+                                                 coefficient=coefficient,
+                                                 operators=operators,
+                                                 default=default,
+                                                 decoder=decoder)
+
+                truth_table.append(values)
+
+            else:
+
+                variables = list(self.variables.keys())
+                coefficients = [variable.coefficient.coefficients for variable in self.variables.values()]
+
+                for combined_coefficients in product(*coefficients):
+                    values = dict(list(zip(variables, combined_coefficients)))
+
+                    values['result'] = self.evaluate(values=values,
+                                                     coefficient=coefficient,
+                                                     operators=operators,
+                                                     default=default,
+                                                     decoder=decoder)
+
+                    truth_table.append(values)
+
+        return DataFrame(truth_table)
