218113a51e794f230f9bfe9c33867690b12f7483
cruz-f <fernandocruz184@gmail.com>
2021-03-24 16:37:57 +0000

    [ADD] Algebra sub-package including parsing symbolic expressions, Expression and Symbolic

--- src/mewpy/algebra/__init__.py
+++ src/mewpy/algebra/__init__.py
@@ -2,4 +2,8 @@ from .expression import Expression
 from .symbolic import *
 
 # TODO: I have implemented an algebra sub-package that allows smooth modeling operations with variables
-#  and algebra expressions of mostly any kind. It is similar to sympy expression handling
+#  and algebra expressions of any kind. It is similar to sympy expression handling. It is also similar to
+#  the boolean syntax parser and probably slower. However, it allows me to handle variables in this expressions
+#  and so dynamically retrieve regulators/genes objects for reactions/interactions/targets in my models.
+#  I don't know if there is an easy way to combine all expression parsing. Thus, I should create a method to create a
+#  Node of a boolean syntax parser from my symbolic mathematics
