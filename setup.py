9503b68579e7988fe77fe4bbe77270d055def56b
Vítor Pereira <vitor.pereira@algoritmi.uminho.pt>
2022-01-14 12:05:17 +0000

    Bump version: 0.1.11 → 0.1.12

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ install_requirements = requirements
 
 setup(
     name='mewpy',
-    version='0.1.11',
+    version='0.1.12',
     python_requires='>=3.6',
     package_dir={'': 'src'},
     packages=find_packages('src'),
--- src/mewpy/__init__.py
+++ src/mewpy/__init__.py
@@ -1,3 +1,3 @@
 __author__ = 'BiSBII CEB University of Minho'
 __email__ = 'vpereira@ceb.uminho.pt'
-__version__ = '0.1.11'
+__version__ = '0.1.12'
