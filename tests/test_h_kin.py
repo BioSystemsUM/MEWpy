75c8dbf58dfeeb0815b0c39e7c20c06dc2737eca
Vítor Pereira <vmsapereira@gmail.com>
2022-05-11 13:36:41 +0100

    [ADD] kinetic test

---
+++ tests/test_h_kin.py
@@ -0,0 +1,24 @@
+from mewpy.simulation.kinetic import KineticSimulation
+import os
+from numpy import linspace, array, dot, isnan
+from collections import OrderedDict
+from scipy.integrate import odeint, solve_ivp
+import unittest
+
+MODELS_PATH = 'tests/data/'
+MODEL = MODELS_PATH + 'chassagnole2002.xml'
+
+
+class TestKineticSimulation(unittest.TestCase):
+
+    def setUp(self):
+        from mewpy.io.sbml import load_ODEModel
+        self.model = load_ODEModel(MODEL)
+
+    def test_build_ode(self):
+        self.model.build_ode()
+
+    def test_simulation(self):
+        from mewpy.simulation.kinetic import KineticSimulation
+        sim = KineticSimulation(self.model)
+        sim.simulate()
