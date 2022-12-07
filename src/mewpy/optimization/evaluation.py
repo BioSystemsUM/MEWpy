2a306c8b110d5d5682da732757ed7156dbabf282
Vítor Pereira <vmsapereira@gmail.com>
2022-12-07 21:54:23 +0000

    [Add] rep_latex

--- src/mewpy/optimization/evaluation.py
+++ src/mewpy/optimization/evaluation.py
@@ -68,6 +68,8 @@ class EvaluationFunction:
     def __str__(self):
         return self.method_str()
 
+
+
     @abstractmethod
     def required_simulations(self):
         return None
@@ -167,6 +169,10 @@ class TargetFlux(PhenotypeEvaluationFunction,KineticEvaluationFunction):
             res = sim.objective_value
 
         return res
+
+    def _repr_latex_(self):
+        sense = '\\max' if self.maximize else '\\min'
+        return "$$ %s %s $$" % (sense, self.reaction.replace('_', '\\_'))
 
     def required_simulations(self):
         """
@@ -297,6 +303,17 @@ class WYIELD(PhenotypeEvaluationFunction):
         except Exception:
             return self.no_solution
 
+    def _repr_latex_(self):
+        sense = '\\max' if self.maximize else '\\min'
+        return "$$ %s \\left( %f \\times FVA_{max}(%s) + (1-%f) \\times FVA_{min}(%s) \\right) $$" % (sense,
+                                                                                                      self.alpha,
+                                                                                                      self.productId.replace(
+                                                                                                          '_', '\\_'),
+                                                                                                      self.alpha,
+                                                                                                      self.productId.replace(
+                                                                                                          '_', '\\_'),
+                                                                                                      )
+
     def required_simulations(self):
         return [self.method]
 
@@ -373,6 +390,18 @@ class BPCY(PhenotypeEvaluationFunction):
             except Exception:
                 print("BPCY No Fluxes")
         return (ssFluxes[self.biomassId] * ssFluxes[self.productId]) / uptake
+
+    def _repr_latex_(self):
+        sense = '\\max' if self.maximize else '\\min'
+        if self.uptakeId:
+            return "$$ %s \\frac{%s \\times %s}{%s} $$" % (sense,
+                                                           self.biomassId.replace('_', '\\_'),
+                                                           self.productId.replace('_', '\\_'),
+                                                           self.uptakeId.replace('_', '\\_'))
+        else:
+            return "$$ %s \\left( %s \\times %s \\right) $$" % (sense,
+                                                                self.biomassId.replace('_', '\\_'),
+                                                                self.productId.replace('_', '\\_'))
 
     def required_simulations(self):
         return [self.method]
