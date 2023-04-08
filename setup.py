e7b9b0571dcdae5d2c907198cc4eec80ef23f0a8
Vítor Pereira <vmsapereira@gmail.com>
2023-04-08 13:59:21 +0100

    Bump version: 0.1.25 → 0.1.26

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ install_requirements = requirements
 
 setup(
     name='mewpy',
-    version='version='0.1.25'',
+    version='0.1.26',
     python_requires='>=3.6',
     package_dir={'': 'src'},
     packages=find_packages('src'),
